1fab9ce411cc889eecf19f4c9760fc4919589ead
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:06:06 +0530

    Create simple calculator c++.c

---
+++ simple calculator c++.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int x,y;
+    int sum;
+    cout << " type a number ";
+    cin >> x;
+    cout << " typr another number is " ;
+    cin >> y;
+    cout << " sum is "<< sum ;
+    return 0;
+}
+
